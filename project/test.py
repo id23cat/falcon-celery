e6821b67d64a43ee41869ac39e65d7e73842a0b1
Aleksey Demidchuk <demidchuk.aleksey@gmail.com>
2018-11-09 18:01:28 +0300

    Celery ready

--- project/test.py
+++ project/test.py
@@ -4,7 +4,9 @@ import unittest
 
 from falcon import testing
 
-from app import app
+from app import app, tasks
+
+from unittest.mock import patch
 
 
 class TestAppRoutes(testing.TestCase):
@@ -16,6 +18,24 @@ class TestAppRoutes(testing.TestCase):
         result = self.simulate_get('/ping')
         self.assertEqual(result.json, 'pong!')
 
+class TestCeleryTasks(unittest.TestCase):
+
+    # def test_fib_task(self):
+    #     self.assertEqual(tasks.fib.run(-1), [])
+    #     self.assertEqual(tasks.fib.run(1), [0, 1])
+    #     self.assertEqual(tasks.fib.run(3), [0, 1, 1, 2])
+    #     self.assertEqual(tasks.fib.run(5), [0, 1, 1, 2, 3, 5])
+
+    @patch('app.tasks.fib')
+    def test_mock_fib_task(self, mock_fib):
+        mock_fib.run.return_value = []
+        self.assertEqual(tasks.fib.run(-1), [])
+        mock_fib.run.return_value = [0, 1]
+        self.assertEqual(tasks.fib.run(1), [0, 1])
+        mock_fib.run.return_value = [0, 1, 1, 2]
+        self.assertEqual(tasks.fib.run(3), [0, 1, 1, 2])
+        mock_fib.run.return_value = [0, 1, 1, 2, 3, 5]
+        self.assertEqual(tasks.fib.run(5), [0, 1, 1, 2, 3, 5])
 
 if __name__ == '__main__':
     unittest.main()
